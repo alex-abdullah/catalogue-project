a8e297529f14c71ff5ad9ab21eb5ca531136d836
AlexAbdullah <alexradja97@gmail.com>
2022-06-03 13:29:02 +1000

    further abstrations and refactoring added

--- Project/Project/Program.cs
+++ Project/Project/Program.cs
@@ -10,38 +10,14 @@ namespace Project
     {
     
         static void Main(string[] args)
-        {
-            Product product = new Product();
+        {           
 
             ShopMethods.InitialWelcome();
             bool programActive = true;
 
-            Catalogue<Hoodie> catalogueHoodie;
-            Catalogue<JumpRopes> catalogueJumpRope;          
-
-            try
-            {                
-                catalogueHoodie = Catalogue<Hoodie>.LoadState("hoodie");
-                Console.WriteLine("Hoodie catalogue successfully loaded.\n");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("No save file found.\n");                
-                catalogueHoodie = new Catalogue<Hoodie>();
+            Catalogue<Hoodie> catalogueHoodie = Hoodie.CatalogueGenerator();
+            Catalogue<JumpRopes> catalogueJumpRope = JumpRopes.CatalogueGenerator();
                 
-            }
-
-            try
-            {
-                catalogueJumpRope = Catalogue<JumpRopes>.LoadState("jumprope");
-                Console.WriteLine("Jump rope catalogue successfully loaded.\n");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("No save file found.\n");
-                catalogueJumpRope = new Catalogue<JumpRopes>();
-            }
-
 
             while (programActive)
             {
@@ -50,113 +26,32 @@ namespace Project
                 string userInput = Console.ReadLine();
 
                 if (userInput == "1")
+                    SelectionOne.DisplayCatalogues(catalogueHoodie, catalogueJumpRope);
+
+
+                if (userInput == "2")
                 {
-                    if (catalogueHoodie.CountItems() <= 0)
-                    {
-                        Console.WriteLine("HOODIE CATALOGUE");
-                        Console.WriteLine("Hoodie catalogue is empty\n");
-                        Console.WriteLine("");
-                    }
-                    else
-                    {
-                        Console.WriteLine("HOODIE CATALOGUE");
-                        Console.WriteLine($"{catalogueHoodie.CatName} \n");
-                        catalogueHoodie.ProductCatalogue.ForEach(Console.WriteLine);
-                        Console.WriteLine("");
-                    }
-
-
-                    if (catalogueJumpRope.CountItems() <= 0)
-                    {
-                        Console.WriteLine("JUMP ROPE CATALOGUE");
-                        Console.WriteLine("Jump rope catalogue is empty\n");
-                        Console.WriteLine("");
-                    }
-                    else
-                    {
-                        Console.WriteLine("JUMP ROPE CATALOGUE");
-                        Console.WriteLine($"{catalogueJumpRope.CatName} \n");
-                        catalogueJumpRope.ProductCatalogue.ForEach(Console.WriteLine);
-                        Console.WriteLine("");
-                    }
-
-                }
-
-                else if (userInput == "2")
-                {
-                    Console.WriteLine("What product do you want to create? \n");
-                    Console.WriteLine("1 - New hoodie");
-                    Console.WriteLine("2 - New jump rope \n");
+                    SelectionTwo.UserPrompts();
                     string userSelection = Console.ReadLine();
 
                     if (userSelection == "1")
-                    {
-                        Console.WriteLine("Enter hoodie NAME:");
-                        string hoodieName = Console.ReadLine();
-
-                        Console.WriteLine("Enter hoodie PRICE:");
-                        string hoodiePrice = Console.ReadLine();
-                        double.Parse(hoodiePrice);
-
-                        Console.WriteLine("Enter hoodie COLOUR:");
-                        string hoodieColour = Console.ReadLine();
-
-                        Console.WriteLine("Enter hoodie SIZE");
-                        string hoodieSize = Console.ReadLine();
-
-
-                        Hoodie hoodieOne = new Hoodie(hoodieName, double.Parse(hoodiePrice), hoodieColour, hoodieSize);
-
-                        catalogueHoodie.Add(hoodieOne);
-
-                        Console.WriteLine($"{hoodieName} has been added! \n");
-
-                        Catalogue<Hoodie>.SaveState(catalogueHoodie, "hoodie");
-                        Console.WriteLine("");
-
-                    }
-
-                    else if (userSelection == "2")
-                    {
-                        Console.WriteLine("Enter rope COLOURWAY NAME:");
-                        string ropeColourway = Console.ReadLine();
-
-                        Console.WriteLine("Enter rope PRICE:");
-                        string ropePrice = Console.ReadLine();
-                        double.Parse(ropePrice);
-
-                        Console.WriteLine("Enter rope LENGTH (feet/inches):");
-                        string ropeLength = Console.ReadLine();
-                        Int32.Parse(ropeLength);
-
-                        JumpRopes ropeOne = new JumpRopes(ropeColourway, double.Parse(ropePrice), Int32.Parse(ropeLength));
-
-                        catalogueJumpRope.Add(ropeOne);
-
-                        Console.WriteLine($"{ropeColourway} has been added! \n");
-
-                        Catalogue<JumpRopes>.SaveState(catalogueJumpRope, "jumprope");
-                    }
-
+                        SelectionTwo.OptionOne(catalogueHoodie);
+                    SelectionTwo.OptionTwo(catalogueJumpRope);
                 }
-                else if(userInput == "3")
+
+                if (userInput == "3")
                 {
-                    Console.WriteLine("Which catalogue do you want\nto delete a product from? \n");
-                    Console.WriteLine("1 - Hoodie catalogue");
-                    Console.WriteLine("2 - Jumprope catalogue \n");
+                    SelectionThree.UserPrompts();
                     string userSelection = Console.ReadLine();
-                    int result = Int32.Parse(userSelection);
+                    
 
                     if (userSelection == "1")
                     {
-                        Catalogue<Hoodie>.DeleteFromCatalogue(catalogueHoodie);
-                        Catalogue<Hoodie>.SaveState(catalogueHoodie, "hoodie");
-
+                        SelectionThree.OptionOne(catalogueHoodie);
                     }
                     else if (userSelection == "2")
                     {
-                        Catalogue<JumpRopes>.DeleteFromCatalogue(catalogueJumpRope);
-                        Catalogue<JumpRopes>.SaveState(catalogueJumpRope, "jumprope");
+                        SelectionThree.OptionTwo(catalogueJumpRope);
                     }
                     else
                     {
@@ -166,23 +61,18 @@ namespace Project
 
                 else if(userInput == "4")
                 {
-                    Console.WriteLine("Which catalogue do you want to clear? \n");
-                    Console.WriteLine("1 - Hoodie catalogue");
-                    Console.WriteLine("2 - Jumprope catalogue \n");
+                    SelectionFour.UserPrompts();
                     string userSelection = Console.ReadLine();
 
                     if (userSelection == "1")
                     {
-                        catalogueHoodie.Empty();
-                        Catalogue<Hoodie>.SaveState(catalogueHoodie, "hoodie");
-                        Console.WriteLine("Hoodie Catalogue is now empty\n");
+                        SelectionFour.OptionOne(catalogueHoodie);
 
                     }
+
                     else if (userSelection == "2")
                     {
-                        catalogueJumpRope.Empty();
-                        Catalogue<JumpRopes>.SaveState(catalogueJumpRope, "jumprope");
-                        Console.WriteLine("Jumprope Catalogue is now empty\n");
+                        SelectionFour.OptionTwo(catalogueJumpRope);
                     }
                     else
                     {
---
+++ Project/Project/SelectionFour.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Project
+{
+    public class SelectionFour
+    {
+        public static void UserPrompts()
+        {
+            Console.WriteLine("Which catalogue do you want to clear? \n");
+            Console.WriteLine("1 - Hoodie catalogue");
+            Console.WriteLine("2 - Jumprope catalogue \n");
+        }
+
+        public static void OptionOne(Catalogue<Hoodie> catalogue)
+        {
+            catalogue.Empty();
+            Catalogue<Hoodie>.SaveState(catalogue, "hoodie");
+            Console.WriteLine("Hoodie Catalogue is now empty\n");
+        }
+
+        public static void OptionTwo(Catalogue<JumpRopes> catalogue)
+        {
+            catalogue.Empty();
+            Catalogue<JumpRopes>.SaveState(catalogue, "Jumprope");
+            Console.WriteLine("Jumprope Catalogue is now empty\n");
+        }
+    }
+}
